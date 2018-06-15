52f6d58b7726984522c419af7a1f0f6505fb4c67
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-06-15 17:15:14 +0300

    first endpoint

---
+++ src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteHandler.java
@@ -0,0 +1,27 @@
+package guru.springframework.webfluxstockquoteservice.web;
+
+import guru.springframework.webfluxstockquoteservice.model.Quote;
+import guru.springframework.webfluxstockquoteservice.service.QuoteGeneratorService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static java.time.Duration.ofMillis;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Component public class QuoteHandler {
+	private final QuoteGeneratorService quoteGeneratorService;
+
+	public QuoteHandler(QuoteGeneratorService quoteGeneratorService) {
+		this.quoteGeneratorService = quoteGeneratorService;
+	}
+
+	public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+		int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+		return ok().contentType(APPLICATION_JSON)
+				.body(quoteGeneratorService.fetchQuoteStream(ofMillis(100)).take(size), Quote.class);
+	}
+}
