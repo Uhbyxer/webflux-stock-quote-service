5473a626484172378f3ab128ea1acd6cae647443
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-06-16 19:43:45 +0300

    mono stream endpoint

--- src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteHandler.java
+++ src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteHandler.java
@@ -5,10 +5,12 @@ import guru.springframework.webfluxstockquoteservice.service.QuoteGeneratorServi
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static java.time.Duration.ofMillis;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component public class QuoteHandler {
@@ -23,5 +25,10 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 
 		return ok().contentType(APPLICATION_JSON)
 				.body(quoteGeneratorService.fetchQuoteStream(ofMillis(100)).take(size), Quote.class);
+	}
+
+	public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+		return ok().contentType(APPLICATION_STREAM_JSON)
+				.body(quoteGeneratorService.fetchQuoteStream(ofMillis(200)), Quote.class);
 	}
 }
--- src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteRouter.java
+++ src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteRouter.java
@@ -7,12 +7,15 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Configuration
 public class QuoteRouter {
-	@Bean public RouterFunction<ServerResponse> route(QuoteHandler quoteHandler) {
-		return RouterFunctions.route(GET("/quotes").and(accept(APPLICATION_JSON)), quoteHandler::fetchQuotes);
+	@Bean
+	public RouterFunction<ServerResponse> route(QuoteHandler quoteHandler) {
+		return RouterFunctions.route(GET("/quotes").and(accept(APPLICATION_JSON)), quoteHandler::fetchQuotes)
+		.andRoute(GET("/quotesStream").and(accept(APPLICATION_STREAM_JSON)), quoteHandler::streamQuotes);
 	}
 }
